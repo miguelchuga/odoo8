4460742dd91ab3e7a7a38b8f6abcab83a5198075
miguelchuga <miguelchuga@gmail.com>
2016-01-20 10:40:13 -0600

    modulo de letras a comunidad

---
+++ invoice_letters/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+#
+# Este modulo tiene reportes personalizados para Guatemala.
+    
+#
+# Status 1.0 - tested on Open ERP 5.0.6
+#
+
+{
+    'name' : 'Numeros a Letras',
+    'version' : '1.0',
+    'category': 'Custom',
+    'description': """Agrega la cantidad en letras a las facturas para la impresion de las mismas""",
+    'author': 'Miguel Chuga',
+    'website': 'http://mcsistemas.net',
+    'depends' : ['base','sale','account'],
+    'init_xml' : [ ],
+    'demo_xml' : [ ],
+    'update_xml' : ["invoice_view.xml"],
+    'installable': True,
+    'certificate': '',
+}
+#,'catalogos_view.xml'
