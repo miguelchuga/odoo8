d8d71d98991fadce665dbcb03812e831bd527597
miguelchuga <miguelchuga@gmail.com>
2016-03-03 20:44:49 -0600

    arreglar y agregar a res_currency

--- res_currency_mc/res_currency.py
+++ res_currency_mc/res_currency.py
@@ -11,9 +11,13 @@ res_currency_rate()
 
 class res_currency(osv.Model):
     
+    def _current_rate_silent(self, cr, uid, ids, name, arg, context=None):
+        return self._get_current_rate(cr, uid, ids, raise_on_no_rate=False, context=context)
+    
     _inherit='res.currency'
     
-    _columns={'rate': fields.float('Rate', digits=(12, 10), help='The rate of the currency to the currency of rate 1'),
+    _columns={'rate_silent': fields.function(_current_rate_silent, string='Current Rate', digits=(12,10),
+                  help='The rate of the currency to the currency of rate 1 (0 if no rate defined).'),
              }
 
 res_currency_rate()
