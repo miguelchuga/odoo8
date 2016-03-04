55c1042771938c8f7c01b91ef88f1f8072abc564
miguelchuga <miguelchuga@gmail.com>
2016-03-03 19:16:47 -0600

    modulo de imagenes

--- res_currency_mc/res_currency.py
+++ res_currency_mc/res_currency.py
@@ -8,3 +8,13 @@ class res_currency_rate(osv.Model):
              }
 
 res_currency_rate()
+
+class res_currency(osv.Model):
+    
+    _inherit='res.currency'
+    
+    _columns={'rate': fields.float('Rate', digits=(12, 10), help='The rate of the currency to the currency of rate 1'),
+             }
+
+res_currency_rate()
+
