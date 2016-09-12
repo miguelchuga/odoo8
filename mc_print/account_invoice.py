5a5f037bf431a053f61d4887400607ccb3b40d90
miguelchuga <miguelchuga@gmail.com>
2016-09-12 06:43:17 -0600

    modifcacion a facturas en invoice_print

--- mc_print/account_invoice.py
+++ mc_print/account_invoice.py
@@ -11,5 +11,5 @@ class account_invoice(models.Model):
 
         assert len(self) == 1, 'para imprimir solo 1 id al mismo tiempo'
         self.sent = True
-
-        return self.env['report'].get_action(self, 'forms_report.report_'+str(self.journal_id.paperformat_id.name)+'_form')
+#        return self.env['report'].get_action(self, 'forms_report.report_'+str(self.journal_id.paperformat_id.name)+'_form')
+        return self.env['report'].get_action(self, self.journal_id.paperformat_id.name)
