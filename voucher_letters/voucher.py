c5f7bfd4453bea863b3347abd8d6d0260f227865
miguelchuga <miguelchuga@gmail.com>
2017-01-31 10:38:39 -0600

    rama 10

--- voucher_letters/voucher.py
+++ voucher_letters/voucher.py
@@ -1,16 +1,17 @@
 # -*- encoding: utf-8 -*-
 from openerp import models,fields,api,exceptions
 from datetime import timedelta,date
-#import util
+import util
 
 class account_voucher(models.Model):
 
-#    @api.one
-#    def _calcular_letras(self):
-#        self.valor_letras =  util.num_a_letras(self.amount)
+    @api.one
+    def _calcular_letras(self):
+        self.valor_letras =  util.num_a_letras(self.amount)
+        print "miguel"
     
     _inherit = 'account.voucher'
-#    valor_letras=fields.Char("Letras",compute=_calcular_letras)
+    valor_letras=fields.Char("Letras",compute=_calcular_letras)
     hecho_por=fields.Char('Hecho por ',size=10)
     revisado_por=fields.Char('Revisado por ',size=10)
     autorizado_por=fields.Char('Autorizado por ',size=10)
