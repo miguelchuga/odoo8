eddd9f8742cd9a4d72b16fbc2b7be9afc0699cda
miguelchuga <miguelchuga@gmail.com>
2017-01-31 09:26:58 -0600

    rama 10

--- invoice_letters/__openerp__.py
+++ invoice_letters/__openerp__.py
@@ -18,7 +18,7 @@
     'init_xml' : [ ],
     'demo_xml' : [ ],
     'update_xml' : ["invoice_view.xml"],
-    'installable': False,
+    'installable': True,
     'certificate': '',
 }
 #,'catalogos_view.xml'
