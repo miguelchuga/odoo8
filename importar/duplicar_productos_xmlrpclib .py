e810a66416645114b345b011f5ae442aaad36fb2
miguelchuga <miguelchuga@gmail.com>
2017-05-15 20:14:54 -0600

    cambios en la nomina....

---
+++ importar/duplicar_productos_xmlrpclib .py
@@ -0,0 +1,55 @@
+#- * -encoding: utf - 8 - * -
+
+import xmlrpclib
+
+username = 'admin' #the user
+pwd = 'Mnbvcxz'      #the password of the user
+dbname = 'o_fsrichard'    #the database
+
+# Get the uid
+sock_common = xmlrpclib.ServerProxy ('http://localhost:5069/xmlrpc/common')
+uid = sock_common.login(dbname, username, pwd)
+
+#replace localhost with the address of the server
+sock = xmlrpclib.ServerProxy('http://localhost:5069/xmlrpc/object')
+
+args = [('company_id', '=',22 )] 
+ids = sock.execute(dbname, uid, pwd, 'product.template', 'search', args)
+
+fields = ['description','codigo_generado','genera_codigo','categ_id','ean13','type', 'list_price','name','default_code','cost_method','valuation','dolar_price','standard_price','default_code','description','currency_id'] 
+
+
+for template in ids:
+
+    args_id = [('id', '=',template )] 
+    ids_t = sock.execute(dbname, uid, pwd, 'product.template', 'search', args_id)
+
+    data = sock.execute(dbname, uid, pwd, 'product.template', 'read', ids_t, fields) 
+    vals_template = {
+       'type': 'product',
+       'list_price':data[0]["list_price"],
+       'name':data[0]["name"],
+       'cost_method':'average',
+       'valuation':'manual_periodic',
+       'dolar_price':data[0]["dolar_price"],
+       'currency_id':data[0]["currency_id"][0],
+       'ean13':data[0]["ean13"],
+       'standard_price':data[0]["standard_price"],
+       'categ_id':data[0]["categ_id"][0],
+#       'taxes_id':  PONER EL ID DE IMPUESTOS DE CLIENTE
+#       'supplier_taxes_id':  PONER EL ID DE IMPUESTOS DE PROVEEDOR
+#       'property_account_income':  PONER EL ID de cuenta x cobrar DE CLIENTE    
+#       'property_account_expense':  PONER EL ID de cuenta de gastos    
+       'genera_codigo':data[0]["genera_codigo"],
+       'codigo_generado':data[0]["codigo_generado"],
+       'description':data[0]["description"],
+       'company_id':20,
+       'description_purchase': data[0]["id"]
+
+        }
+
+    template_id = sock.execute(dbname, uid, pwd, 'product.template', 'create', vals_template)
+
+
+
+
--- importar/importar_productos.py
+++ importar/importar_productos.py
@@ -50,4 +50,49 @@ for cadena in reader:
     index = index + 1
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for p in product_ids:
+    producto = template_obj.search_read([('id', '=', p)])
+    vals_template = {
+       'type': 'product',
+       'list_price':producto[0]["list_price"],
+       'name':producto[0]["name"],
+       'default_code':producto[0]["default_code"],
+       'cost_method':producto[0]["cost_method"],
+       'valuation':producto[0]["valuation"],
+       'dolar_price':producto[0]["dolar_price"],
+       'currency_id':producto[0]["currency_id"][0],
+       'ean13':producto[0]["ean13"],
+       'standard_price':producto[0]["standard_price"],
+       'categ_id':producto[0]["categ_id"][0],
+#       'taxes_id':  PONER EL ID DE IMPUESTOS DE CLIENTE
+#       'supplier_taxes_id':  PONER EL ID DE IMPUESTOS DE PROVEEDOR
+#       'property_account_income':  PONER EL ID de cuenta x cobrar DE CLIENTE    
+#       'property_account_expense':  PONER EL ID de cuenta de gastos    
+       'genera_codigo':producto[0]["genera_codigo"],
+       'default_code':producto[0]["default_code"],
+       'codigo_generado':producto[0]["codigo_generado"],
+       'description':producto[0]["description"],
+       'company_id':20,
+    }   
+    template_id = template_obj.create(vals_template)
+    print "miguel"
+
+
+
+
+
+
